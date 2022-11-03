c4df17018eddd580bf685ba33c8121605763c53c
Jenn Scheer <singleportrait@gmail.com>
2022-11-03 13:30:10 -0400

    Add (test) Google Maps input to Sanity admin
    I haven't figured out a way to use this plugin without committing the
    API key to the repo, so this may have to be hand-rolled.

--- studio/schemas/documents/restaurant.js
+++ studio/schemas/documents/restaurant.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import {format, parse} from 'date-fns';
+
 export default {
   name: 'restaurant',
   title: 'Restaurant',
@@ -28,5 +31,41 @@ export default {
       title: 'Article Url',
       type: 'url',
     },
+    {
+      name: 'contributor',
+      title: 'Article Contributor',
+      type: 'string',
+    },
+    {
+      name: 'location',
+      title: 'Location',
+      type: 'geopoint',
+    },
   ],
+  orderings: [
+    {
+      title: 'Pub date, new to old',
+      name: 'publicationDateDesc',
+      by: [{field: 'publicationDate', direction: 'desc'}],
+    },
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      subtitle: 'articleTitle',
+      date: 'publicationDate',
+      location: 'location',
+    },
+    prepare(selection) {
+      const {title, subtitle, date, location} = selection;
+      const formattedDate = date ? format(new Date(parse(date, 'yyyy-MM-dd', new Date())), 'MM/dd') : undefined;
+      const locationStatus = location ? <span>{date && <br />}📌</span> : '';
+      return {
+        title,
+        // subtitle: `${formattedDate || ''}${(formattedDate && subtitle) ? ': ' : ''}${subtitle || ''}`,
+        subtitle,
+        media: <small style={{textAlign: 'center'}}>{formattedDate}{locationStatus}</small>,
+      };
+    },
+  },
 };
