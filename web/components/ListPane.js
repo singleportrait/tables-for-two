42c3316cef9037179fc4d4645a48c8ab22e7f675
Jenn Scheer <singleportrait@gmail.com>
2022-11-22 09:33:38 -0500

    Separate ListPane out into its own component

---
+++ web/components/ListPane.js
@@ -0,0 +1,41 @@
+import classNames from 'classnames';
+
+import { formatSanityDate } from '../helpers/dates';
+
+import Button from './Button';
+
+const ListPane = ({ restaurants, openRestaurant, onClose }) => (
+  <div className="absolute bottom-0 right-0 top-0 w-full">
+    <Button onClick={onClose} className="absolute right-4 top-20">
+      Close
+    </Button>
+    <div style={{ height: 'calc(100vh - 7rem)' }} className="relative mt-28 bg-background border-t border-secondary p-4 w-full overflow-y-scroll">
+      {restaurants.map((restaurant, i) => (
+        <button
+          type="button"
+          key={restaurant._id}
+          onClick={() => openRestaurant(restaurant)}
+          className={classNames({
+            'flex items-center space-x-4 justify-between cursor-pointer w-full text-left group': true,
+            'pt-3': i > 0,
+            'border-b border-secondary pb-3': i !== restaurants.length - 1,
+          })}
+        >
+          <div>
+            <h2 className="antialiased group-hover:text-primary">
+              {restaurant.name}
+            </h2>
+            <small className="text-xs font-mono text-slate-800">
+              {formatSanityDate(restaurant.article?.issueDate)} Issue
+            </small>
+          </div>
+          <Button>
+            Open
+          </Button>
+        </button>
+      ))}
+    </div>
+  </div>
+);
+
+export default ListPane;
--- web/components/MapWithOverlay.js
+++ web/components/MapWithOverlay.js
@@ -1,14 +1,12 @@
 import { useState, useEffect } from 'react';
 import { GoogleMap } from '@react-google-maps/api';
 import { PortableText } from '@portabletext/react';
-import classNames from 'classnames';
-
-import { formatSanityDate } from '../helpers/dates';
 
 import CustomMarker from './CustomMarker';
 import RestaurantPane from './RestaurantPane';
 import Button from './Button';
 import ButtonLink from './ButtonLink';
+import ListPane from './ListPane';
 
 const defaultZoom = 13;
 
@@ -157,37 +155,11 @@ const MapWithOverlay = ({
       )}
       {/* Open list view */}
       {openPane === 'list' && (
-        <div className="absolute bottom-0 right-0 top-0 w-full">
-          <Button onClick={() => setOpenPane()} className="absolute right-4 top-20">
-            Close
-          </Button>
-          <div style={{ height: 'calc(100vh - 7rem)' }} className="relative mt-28 bg-background border-t border-secondary p-4 w-full overflow-y-scroll">
-            {restaurants.map((restaurant, i) => (
-              <button
-                type="button"
-                key={restaurant._id}
-                onClick={() => openRestaurant(restaurant)}
-                className={classNames({
-                  'flex items-center space-x-4 justify-between cursor-pointer w-full text-left group': true,
-                  'pt-3': i > 0,
-                  'border-b border-secondary pb-3': i !== restaurants.length - 1,
-                })}
-              >
-                <div>
-                  <h2 className="antialiased group-hover:text-primary">
-                    {restaurant.name}
-                  </h2>
-                  <small className="text-xs font-mono text-slate-800">
-                    {formatSanityDate(restaurant.article?.issueDate)} Issue
-                  </small>
-                </div>
-                <Button>
-                  Open
-                </Button>
-              </button>
-            ))}
-          </div>
-        </div>
+        <ListPane
+          restaurants={restaurants}
+          onClose={() => setOpenPane()}
+          openRestaurant={openRestaurant}
+        />
       )}
       {/* Open info pane */}
       {openPane === 'info' && (
